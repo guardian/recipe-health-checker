07e969a3a1c7f7ab122c5cb887c6ddde8cf37379
Andy Gallagher <fredex42@gmail.com>
2025-06-11 13:40:45 +0100

    basic setup customisatipn

--- evaluation-ui/src/main.tsx
+++ evaluation-ui/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world</div>
+    }
+]);
+
+const theme = createTheme();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+      <CssBaseline>
+          <ThemeProvider theme={theme}>
+            <RouterProvider router={router}/>
+          </ThemeProvider>
+      </CssBaseline>
+  </StrictMode>
 )
